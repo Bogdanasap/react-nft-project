a1dd49bf7277ea50e85dd6410dba52a171142cfa
Bogdan Gabriel Lazar <B.G.Lazar@student.tudelft.nl>
2022-02-17 15:42:21 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/App.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header";
+import RoadMap from "./Components/RoadMap";
 
 function App() {
   return (
       <div className='app'>
         <Header />
-
+        <RoadMap />
       </div>
   )
 }
--- src/Components/RoadMap.js
+++ src/Components/RoadMap.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import './road.css'
-import punkLogo from '../assets/header/cryptopunk-logo.png'
-import searchIcon from '../assets/header/search.png'
-import themeSwitchIcon from '../assets/header/theme-switch.png'
 import bone from'../assets/bone-removebg-preview.png'
 
 const Road = () => {
     return (
         <div className='road-map'>
-            <div>
-            <span styles={{backgroundImage:`url(${bone})`}} className="dot"></span>
-            <span className="dot"></span>
-            <span className="dot"></span>
-            <span className="dot"></span>
+           <div className='dot'>
+               <img src={bone}/>
+           </div>
+
+            <div className='dot'>
+                <img src={bone}/>
             </div>
+
+            <div className='dot'>
+                <img src={bone}/>
+            </div>
+
+            <div className='dot'>
+                <img src={bone}/>
+            </div>
+
         </div>
     )
 

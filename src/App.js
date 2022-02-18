9e9df7f6934be4ce11662abc28f6975ce2240796
Bogdan Gabriel Lazar <B.G.Lazar@student.tudelft.nl>
2022-02-18 09:31:46 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/App.css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from "./Components/Header";
 import RoadMap from "./Components/RoadMap";
 import Trailer from "./Components/Trailer.js";
 import Footer from "./Components/Footer";
+import Team from "./Components/Team"
+import Faq from "./Components/Faq"
+import About from "./Components/About"
 
 
 function App() {
---
+++ src/Components/Faq.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./faq.css"
+
+const Faq = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Faq

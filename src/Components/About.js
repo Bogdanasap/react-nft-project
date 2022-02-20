dce427fb4b52587159565be9e70a7c67f993ba11
Bogdan Gabriel Lazar <B.G.Lazar@student.tudelft.nl>
2022-02-20 21:12:17 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/App.css

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,27 +4,30 @@ import './about.css'
 const About = () => {
     return (
         <div className='stories'>
-            <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
-            <div className="story1">
-                <p>
-                    WHAT IS DRIPPYDOBS?
-                    DrippyDobs is a collection of 10,000 Dobberman NFTs.
-                    When the DobberLads team were having a drink in the laboratory, they all started imagining a world populated by the most fashionable dogs, Dobermans.
-                    They kept trying to find a way to tell people about their vision but couldn't find a way to do so. At some point, they finally got it.
-                    What if they created a virtual world full of beautiful dobermans and put it on a blockchain so that every dog enthusiast could get their very own drippy doberman.
-                </p>
+            <div className='box1'>
+                <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet"/>
+                <div className="story1">
+
+                        <h2>WHAT IS DRIPPYDOBS?</h2>
+                    <p>
+                        DrippyDobs is a collection of 10,000 Dobberman NFTs.
+                        When the DobberLads team were having a drink in the laboratory, they all started imagining a world populated by the most fashionable dogs, Dobermans.
+                        They kept trying to find a way to tell people about their vision but couldn't find a way to do so. At some point, they finally got it.
+                        What if they created a virtual world full of beautiful dobermans and put it on a blockchain so that every dog enthusiast could get their very own drippy doberman.
+                    </p>
+                </div>
             </div>
-
-             <div className="story2">
-                <p>
-
-                    WHAT IS DOBBERLADS?
-                    DobberLads is a team of 5 guys that share a passion for dogs.
-                    Our mission is to bring Dobbermans to the ERC-271 blockchain and give every one of you a chance to own your very own custom Doberman NFT.
-                    We will bring our NFTs to the Metaverse with an app ready to offer functionality and battle procrastination.
-                    We have nothing but good intentions for our fans and the environment and we'd love to welcome you guys in our community.
-                </p>
-            </div> */
+            <div className='box2'>
+                 <div className="story2">
+                       <h2>WHAT IS DOBBERLADS?</h2>
+                     <p>
+                        DobberLads is a team of 5 guys that share a passion for dogs.
+                        Our mission is to bring Dobbermans to the ERC-271 blockchain and give every one of you a chance to own your very own custom Doberman NFT.
+                        We will bring our NFTs to the Metaverse with an app ready to offer functionality and battle procrastination.
+                        We have nothing but good intentions for our fans and the environment and we'd love to welcome you guys in our community.
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }

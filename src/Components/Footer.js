16d9d0cb674b0f036c17b50df95497be04d87e30
Boboc Robert Andrei <andreiboboccibo@gmail.com>
2022-03-10 00:27:33 +0200

    Cookies

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import './footer.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import discordIcon from "../assets/assets_website/discord-black-icon-703937.png";
 import instagramIcon from "../assets/assets_website/5ecec78673e4440004f09e77.png";
 import twitterIcon from "../assets/assets_website/pngwing.com.png";
 import dubbsLogo from "../assets/header/img.png";
+import {Col, Container, Row} from "react-bootstrap";
 
 const Footer = () => {
 
@@ -29,31 +31,31 @@ const Footer = () => {
 
     return (
         <div className='footer'>
-            <div className='footerLogoContainer' onClick={moveToTop}>
-                <img src={dubbsLogo} className='footerLogo' />
-            </div>
-
-            <div className='footerActions'>
-
-                <div className='discordContainer' onClick={moveToDiscord}>
+            <Container>
+            <Row>
+                <Col><div className='footerLogo' onClick={moveToTop}>
+                    <img src={dubbsLogo} className='footerLogo' />
+                </div></Col>
+                <Col><div className='discordContainer ' onClick={moveToDiscord}>
                     <img src={discordIcon}/>
                     <a href="https://discord.com/"></a>
                 </div>
 
-                <div className='instagramContainer' onClick={moveToInsta}>
-                    <img src={instagramIcon}/>
-                </div>
+                    <div className='instagramContainer' onClick={moveToInsta}>
+                        <img src={instagramIcon}/>
+                    </div>
 
-                <div className='twitterContainer' onClick={moveToTwitter}>
-                    <img src={twitterIcon}/>
-                </div>
-
-            </div>
-            <button onClick={moveToMint} className='mintButton'>
-                <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet"/>
-                Mint
-            </button>
+                    <div className='twitterContainer' onClick={moveToTwitter}>
+                        <img src={twitterIcon}/>
+                    </div></Col>
+                <Col><button onClick={moveToMint} className='mintButton'>
+                    <link href="https://fonts.googleapis.com/css2?family=Annie+Use+Your+Telescope&display=swap" rel="stylesheet"/>
+                    Mint
+                </button></Col>
+            </Row>
+            </Container>
         </div>
+
     )
 }
 
